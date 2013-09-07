69415abe47837ed87dc715f18c0cf9983be09385
Formal codes goto: tomxue <tomxue@outlook.com>
2013-09-07 12:41:40 +0800

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,10 @@
+import functools
+import threading
+import time
+ 
+def hello(name):  
+    print 'hello world, i am %s, Current time: %s' % (name, time.time())  
+       
+hello = functools.partial(hello, 'guo')
+t = threading.Timer(3, hello)
+t.start()
